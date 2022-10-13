60550118823c41d88075c3791bf18b2d8523af1a
lu-rios97 <danielriver492@gmail.com>
2022-10-13 09:52:45 -0500

    arreglo

--- model/conexion.php
+++ model/conexion.php
@@ -1,12 +1,12 @@
 <?php 
-$contrasena = "";
-$usuario = "root";
-$nombre_bd = "crud";
+$contrasena = "yhrl2Gnr1Y4DDswbjilx";
+$usuario = "uixlomrvj0wuf5wx";
+$nombre_bd = "baugdpok0nt0ypdbsita";
 
 try {
 	$bd = new PDO (
-		'mysql:host=localhost;
-		dbname='.$nombre_bd,
+		'mysql:host=baugdpok0nt0ypdbsita-mysql.services.clever-cloud.com;
+		baugdpok0nt0ypdbsita='.$nombre_bd,
 		$usuario,
 		$contrasena,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
