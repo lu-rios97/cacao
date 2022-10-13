475e42eda2f5f636e35f244a200b57d248ab076a
lu-rios97 <danielriver492@gmail.com>
2022-10-13 10:39:05 -0500

    conexion

--- model/conexion.php
+++ model/conexion.php
@@ -1,12 +1,12 @@
 <?php 
-$contrasena = "9r1vJdpMbzJrf1md45gg";
-$usuario = "uhuokqqyezux4t8n";
-$nombre_bd = "beqwb29khwketxj13qor";
+$contrasena = "c1BJQEZor84xbtKcyXZS";
+$usuario = "u96tjwuzms1vaze0";
+$nombre_bd = "bv9rawwwx8j4ytmlgh58";
 
 try {
 	$bd = new PDO (
-		'mysql:host=beqwb29khwketxj13qor-mysql.services.clever-cloud.com;
-		beqwb29khwketxj13qor='.$nombre_bd,
+		'mysql:host=bv9rawwwx8j4ytmlgh58-mysql.services.clever-cloud.com;
+		bv9rawwwx8j4ytmlgh58='.$nombre_bd,
 		$usuario,
 		$contrasena,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
